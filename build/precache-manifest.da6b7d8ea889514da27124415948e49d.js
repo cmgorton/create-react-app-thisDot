51e9dfde10f0ac6f7bd94629cab283eb08a0af9f
Christina <chmagorton@gmail.com>
2020-06-18 15:00:14 -0600

    deploying to netlify

---
+++ build/precache-manifest.da6b7d8ea889514da27124415948e49d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a01dd7c2275a1593c4a2fcf3bacb9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b07d3abe2f9db5167ba",
+    "url": "/static/css/main.33affcce.chunk.css"
+  },
+  {
+    "revision": "28961c4b766248a0fa9a",
+    "url": "/static/js/2.432bb591.chunk.js"
+  },
+  {
+    "revision": "6b07d3abe2f9db5167ba",
+    "url": "/static/js/main.6a1d5f2c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,50 +1,84 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { useState, useEffect } from "react";
+import UserList from "./components/UserList";
+import Pagination from "./components/Pagination";
+import Octocat from "./components/Octocat";
+import axios from "axios";
+import "./styles.css";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+function App() {
+  const [response, setResponse] = useState([]);
+  const [count, setCount] = useState("");
+  const [query, setQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(6);
 
-  handleClick = api => e => {
+
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
+  useEffect(() => {
+    const data = localStorage.getItem("users", "results");
+    if (data) {
+      setResponse(JSON.parse(data));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(response));
+    localStorage.setItem("results", JSON.stringify(count));
+  });
+
+
+  const formSubmit = e => {
     e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
+    axios.get(`https://api.github.com/search/users?q=${query}&page=2&limit=14`)
+      .then(response => {
+        const data = response.data.items;
+        setResponse(data);
+        setCount(response.data.total_count);
+      })
+      .catch(errors => {
+        console.error(errors);
+      });
   }
 
-  render() {
-    const { loading, msg } = this.state
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = response.slice(indexOfFirstPost, indexOfLastPost);
 
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+  return (
+    <div className="main">
+      <h1>Github Search</h1>
+      <Pagination
+        currentPage={currentPage}
+        postsPerPage={postsPerPage}
+        totalPosts={response.length}
+        paginate={paginate}
+      />
+      <div className="cat">
+        <Octocat />
       </div>
-    )
-  }
+      <form onSubmit={formSubmit}>
+        <input
+          type="text"
+          id="input"
+          placeholder="user name..."
+          onChange={handleInputChange}
+          value={query}
+          aria-label="user name"
+        />
+        <button id="button">
+          Find a user
+        </button>
+      </form>
+      <h2>{count} results</h2>
+      <UserList response={currentPosts} />
+    </div>
+  );
 }
 
-export default App
+export default App;
